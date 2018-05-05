892c0f218646f95b33dd063882fe2d7fe48af145
Cassandra King <puddincat@gmail.com>
2018-05-05 17:11:38 -0400

    update center to san juan , change  basemap

--- src/App.js
+++ src/App.js
@@ -14,19 +14,21 @@ class App extends React.PureComponent {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>esri-loader-react test app</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            esri-loader-react test
         </p>
-        <EsriLoaderReact 
-          options={options} 
-          modulesToLoad={['esri/Map', 'esri/views/MapView']}    
+        <EsriLoaderReact
+          options={options}
+          modulesToLoad={['esri/Map', 'esri/views/MapView']}
           onReady={({loadedModules: [Map, MapView], containerNode}) => {
             new MapView({
               container: containerNode,
-              map: new Map({basemap: 'topo'})
+              map: new Map({basemap: 'national-geographic'}),
+              center: [-66.105721, 18.466333],
+              zoom: 15,
+
             })
           }}
         />
